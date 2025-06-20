86a973c696fdf3365c130b354410fafe2ba47e2b
ragizz <84837760+ragizz@users.noreply.github.com>
2025-06-20 16:44:49 +0545

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Prajwal Regmi';
+export const SITE_DESCRIPTION = 'Built with Astro and TailwindCSS';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
