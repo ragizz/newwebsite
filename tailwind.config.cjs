0d372cbb113df332b7bf259b7aafdfd966e5de22
ragizz <84837760+ragizz@users.noreply.github.com>
2025-06-20 22:34:52 +0545

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,5 +9,9 @@ module.exports = {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+	daisyui: {
+            themes: true,
+  	    darkTheme: "cupcake"
+}
 	  }
 }
